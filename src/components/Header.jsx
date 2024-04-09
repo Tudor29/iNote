2ed9fe89586175a9aee21bd96b6deb453119f542
Tudor Birsan <tudorbirsan@Tudors-MacBook-Air.local>
2024-04-09 19:29:38 +0300

    app

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LightbulbOutlinedIcon fontSize="large" /> iNote
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
